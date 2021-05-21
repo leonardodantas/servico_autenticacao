6449c4a336cc1a456425a64c481b1dda95c44786
leonardo <leonardo1317@yahoo.com.br>
2021-05-21 08:03:58 -0300

    Criação dos profiles

--- src/main/java/com/servico/autenticacao/Teste.java
+++
@@ -1,4 +0,0 @@
-package com.servico.autenticacao;
-
-public class Teste {
-}
--- src/main/java/com/servico/autenticacao/config/SwaggerConfig.java
+++ src/main/java/com/servico/autenticacao/config/SwaggerConfig.java
@@ -3,6 +3,7 @@ package com.servico.autenticacao.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
@@ -12,6 +13,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableSwagger2
 @Configuration
+@Profile({"dev","hml"})
 public class SwaggerConfig {
 
     @Value("${spring.application.name}")
